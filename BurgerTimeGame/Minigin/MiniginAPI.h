f20128e07780548e1a0c1f92a4e7cae5e0bacd0a
GianniWalraet <gianni.walraet@student.howest.be>
2022-03-30 10:52:05 +0200

    SOme changes

---
+++ BurgerTimeGame/Minigin/MiniginAPI.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Singleton.h"
+#include "Timer.h"
+
+#include "Command.h"
+
+#include "BaseComponent.h"
+#include "FPSComponent.h"
+#include "HealthDisplayComponent.h"
+//#include "PeterPepperComponent.h"
+#include "RenderComponent.h"
+#include "ScoreDisplayComponent.h"
+#include "SpriteComponent.h"
+#include "TextComponent.h"
+#include "TextureComponent.h"
+#include "TransformComponent.h"
+
+#include "Font.h"
+#include "Texture2D.h"
+
+#include "ControllerInput.h"
+#include "InputManager.h"
+#include "SDLKeyboardInput.h"
+#include "XBox360Controller.h"
+
+#include "AchievementObserver.h"
+#include "Observer.h"
+#include "Subject.h"
+
+#include "GameObject.h"
+#include "Scene.h"
+#include "SceneManager.h"
+
+#include "Renderer.h"
+#include "ResourceManager.h"
+#include "MiniginPCH.h"
